f30cf432f5d8cd90086eda90629ed11fc175bd2d
Vanessa Roy <vr.vanessa.roy@gmail.com>
2023-06-13 19:54:35 +0200

    add the function 5% discount for regular user and fix sql request getTicket

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,7 +5,7 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-	public void calculateFare(Ticket ticket) {
+	public void calculateFare(Ticket ticket, boolean discount) {
 		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
 			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 		}
@@ -17,6 +17,19 @@ public class FareCalculatorService {
 
 		if (duration < 0.5) {
 			ticket.setPrice(0);
+		} else if (discount) {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR * 0.95);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR * 0.95);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
 		} else {
 			switch (ticket.getParkingSpot().getParkingType()) {
 			case CAR: {
@@ -31,5 +44,9 @@ public class FareCalculatorService {
 				throw new IllegalArgumentException("Unkown Parking Type");
 			}
 		}
+	}
+
+	public void calculateFare(Ticket ticket) {
+		calculateFare(ticket, false);
 	}
 }
